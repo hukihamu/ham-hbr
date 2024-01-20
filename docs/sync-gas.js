bc860411e6e80287a13302d5da1889a787204303
hukihamu <8084194+hukihamu @users.noreply.github.com>
2024-01-20 12:42:37 +0900

    Move async fetch operation inside 'load' event listener
    The code for fetching 'env.json' has been moved inside the 'load' event listener to ensure asynchronous operations are performed only after the window has been fully loaded. This allows more efficient handling of asynchronous fetch calls, thus improving the script execution performance.

--- docs/sync-gas.js
+++ docs/sync-gas.js
@@ -1,19 +1,21 @@
-const env = await fetch('env.json').then(it => it.json())
 
-window.addEventListener('message', (event) => {
-  if (event.data?.type === 'after-each') {
-    const route = JSON.parse(event.data.value)
-    const query = Object.keys(route.query).reduce((previousValue, currentValue) => {
-      if (previousValue) {
-        previousValue += '&'
-      } else {
-        previousValue += '?'
-      }
-      return previousValue + `${currentValue}=${route.query[currentValue]}`
-    }, '')
-    history.pushState({}, '',`${env.rootPath + query}#${route.path}` )
-  }
-})
-window.addEventListener('load', () => {
+window.addEventListener('load', async () => {
+  const env = await fetch('env.json').then(it => it.json())
+
+  window.addEventListener('message', (event) => {
+    if (event.data?.type === 'after-each') {
+      const route = JSON.parse(event.data.value)
+      const query = Object.keys(route.query).reduce((previousValue, currentValue) => {
+        if (previousValue) {
+          previousValue += '&'
+        } else {
+          previousValue += '?'
+        }
+        return previousValue + `${currentValue}=${route.query[currentValue]}`
+      }, '')
+      history.pushState({}, '',`${env.rootPath + query}#${route.path}` )
+    }
+  })
+
   document.getElementById('gas-frame').src = `${env.gasURL}#${location.pathname.replace(env.rootPath, '')}`
 })
